25ed321b087da875feb2bd5be2cd27b585c3cb9f
aundreka <aundreka@outlook.com>
2026-01-30 14:19:14 +0800

    wrapped /news in suspense boundary

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,11 @@
 // app/news/page.tsx
 import NewsPageClient from "@/components/news/NewsPageClient";
+import { Suspense } from "react";
 
 export default function NewsPage() {
-  return <NewsPageClient />;
+  return (
+    <Suspense fallback={null}>
+      <NewsPageClient />
+    </Suspense>
+  );
 }
